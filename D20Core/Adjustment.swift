875514b3c590970f6e92ee82ddf922e5cfa71fb2
John McCormick <jmccormick@blizzard.com>
2018-09-30 14:28:32 -0700

    introduce Adjustment structure

---
+++ D20Core/Adjustment.swift
@@ -0,0 +1,25 @@
+//
+//  Adjustment.swift
+//  D20Core
+//
+//  Created by John McCormick on 9/30/18.
+//  Copyright © 2018 John McCormick. All rights reserved.
+//
+
+import Foundation
+
+enum AdjustmentCategory {
+    case inherent, equipment, effect
+}
+
+struct Adjustment {
+    var value : Int
+    let category : AdjustmentCategory
+    let name : String
+    
+    init(name : String, modifierValue : Int, category : AdjustmentCategory) {
+        self.name = name
+        value = modifierValue
+        self.category = category
+    }
+}
