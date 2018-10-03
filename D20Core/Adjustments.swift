259f77fc47bfa6dbc776c5a9ce6642ee7c65aeed
John McCormick <jmccormick@blizzard.com>
2018-10-02 18:08:23 -0700

    rework scheme for adjustment change notification - unit tests in place

--- D20Core/Adjustments.swift
+++ D20Core/Adjustments.swift
@@ -8,21 +8,54 @@
 
 import Foundation
 
+protocol Disposable {
+    func dispose()
+}
+
+protocol AdjustmentListener {
+    func adjustmentChanged(adjustment: Adjustment, modifierDelta: Int)
+    func apply(adjustment: Adjustment)
+}
+
 enum AdjustmentCategory {
     case inherent, equipment, effect
 }
 
 class Adjustment : Hashable {
     
-    var value : Int
+    var value: Int = 0 {
+        willSet(newValue) {
+            
+        }
+        didSet {
+            if value != oldValue {
+                let delta = value - oldValue
+                notifyOfChange(delta: delta)
+            }
+        }
+    }
     let category : AdjustmentCategory
     let name : String
+    fileprivate var subscribers = [AdjustmentEventHandler]()
     
     init(name : String, modifierValue : Int, category : AdjustmentCategory) {
         self.name = name
         value = modifierValue
         self.category = category
-        
+    }
+    
+    private func notifyOfChange(delta: Int) {
+        print("Notify of change on \(self.name) with delta = \(delta) to subscribers [\(subscribers.count)]")
+        for handler in subscribers {
+            handler.handle(adjustment: self, modifierDelta: delta)
+        }
+    }
+    
+    func subscribe(listener: AdjustmentListener) -> Disposable {
+        let handler = AdjustmentEventHandler(adjustment: self, handler: listener)
+        subscribers.append(handler)
+        listener.apply(adjustment: self)
+        return handler
     }
     
     static func == (lhs: Adjustment, rhs: Adjustment) -> Bool {
@@ -35,7 +68,22 @@ class Adjustment : Hashable {
     }
 }
 
-protocol AdjustmentListener {
-    var trackedModifiers: Int {get set}
-    mutating func adjustmentChanged(adjustment: Adjustment, modifierDelta: Int)
+private class AdjustmentEventHandler : Disposable {
+    let adjustment: Adjustment
+    let handler: AdjustmentListener
+    
+    init(adjustment: Adjustment, handler: AdjustmentListener) {
+        self.adjustment = adjustment
+        self.handler = handler
+    }
+    
+    func dispose() {
+        adjustment.subscribers = adjustment.subscribers.filter { $0 !== self }
+    }
+    
+    func handle(adjustment: Adjustment, modifierDelta: Int) {
+        handler.adjustmentChanged(adjustment: adjustment, modifierDelta: modifierDelta)
+    }
 }
+
+
--- D20CoreTests/AdjustmentsTests.swift
+++ D20CoreTests/AdjustmentsTests.swift
@@ -13,7 +13,13 @@ class TestListener : AdjustmentListener {
     var trackedModifiers: Int = 0
 
     func adjustmentChanged(adjustment: Adjustment, modifierDelta: Int) {
+        print("notified of adjustment change to \(adjustment.name), delta = \(modifierDelta)")
         trackedModifiers += modifierDelta
+    }
+    
+    func apply(adjustment: Adjustment) {
+        print("applying and tracking adjustment \(adjustment.name) with initial modifier of \(adjustment.value)")
+        trackedModifiers += adjustment.value
     }
 }
 
@@ -22,6 +28,7 @@ class AdjustmentsTests : XCTestCase {
     func testAdjustmentInitialization() {
         let testAdjustment = Adjustment(name: "testAdjustment", modifierValue: 5, category: AdjustmentCategory.inherent)
         XCTAssertTrue(testAdjustment.value == 5 , "adjustment value base equals initialized value")
+        testAdjustment.value = 7
     }
 
     func testInitialization() {
@@ -35,5 +42,14 @@ class AdjustmentsTests : XCTestCase {
         XCTAssertTrue(listener.trackedModifiers == 4 , "trackedModifiers equals current+delta value")
         listener.adjustmentChanged(adjustment: Adjustment(name: "a", modifierValue: 2, category: AdjustmentCategory.inherent), modifierDelta: -2)
         XCTAssertTrue(listener.trackedModifiers == 2 , "trackedModifiers equals current+delta value")
+    }
+    
+    func testAdjustmentSubscription() {
+        let testAdjustment = Adjustment(name: "testAdjustment", modifierValue: 5, category: AdjustmentCategory.inherent)
+        var listener : TestListener = TestListener()
+        let handler = testAdjustment.subscribe(listener: listener)
+        testAdjustment.value = 7
+        XCTAssertTrue(listener.trackedModifiers == testAdjustment.value , "trackedModifiers equals change in testAdjustment.value")
+        handler.dispose()
     }
 }
